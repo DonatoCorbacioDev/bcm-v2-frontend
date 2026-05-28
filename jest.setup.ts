f6048a163870a91ed59ace842dabb9d9f5bd40d0
DESKTOP-Q5CMCK9\Donato <donatocorbacio92@gmail.com>
2026-05-28 08:42:41 +0200

    fix(sonar): resolve S1186 empty methods and S7764 globalThis, exclude app/ from coverage

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,8 +2,8 @@ import "@testing-library/jest-dom";
 
 // Polyfill ResizeObserver for Radix UI components that use @radix-ui/react-use-size
 class ResizeObserverMock {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
+  observe() { /* no-op mock */ }
+  unobserve() { /* no-op mock */ }
+  disconnect() { /* no-op mock */ }
 }
-global.ResizeObserver = ResizeObserverMock;
+globalThis.ResizeObserver = ResizeObserverMock;
